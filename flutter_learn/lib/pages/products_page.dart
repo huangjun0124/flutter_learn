3ee0d83dd94f7ecbdfe0b0c89b9fe10d632d9cde
huangjun <keithhuang0124@gmail.com>
2019-04-21 14:13:39 +0800

    添加一个侧边栏： * 使用 Drawer

--- flutter_learn/lib/pages/products_page.dart
+++ flutter_learn/lib/pages/products_page.dart
@@ -2,12 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 import '../cat_manager.dart';
+import './cat_edit.dart';
 
 class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              automaticallyImplyLeading: false,
+              title: Text('Choose'),),
+            ListTile(title: Text('Manage Cats'),onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (BuildContext context)=>EditCatPage()));
+            },)
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text('LovingCat'),
       ),
