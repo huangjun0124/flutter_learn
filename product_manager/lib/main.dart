d0e6b3192f548e46c8deac1658e4e1b77686e746
huangjun <keithhuang0124@gmail.com>
2019-04-21 15:12:24 +0800

    add 'Product_Manage' project, switch to this one afterwards

---
+++ product_manager/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import './pages/products_admin.dart';
+import './pages/products.dart';
+
+void main() {
+  // debugPaintSizeEnabled = true;
+  // debugPaintBaselinesEnabled = true;
+  // debugPaintPointersEnabled = true;
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // debugShowMaterialGrid: true,
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.deepPurple),
+      // home: AuthPage(),
+      routes: {
+        '/': (BuildContext context) => ProductsPage(),
+        '/admin': (BuildContext context) => ProductsAdminPage(),
+      },
+    );
+  }
+}
